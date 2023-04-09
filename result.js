0c4290c7e12dbc987b7ad1fedf1117f3823cf01a
mohammad zayed <mohammadzayed193@gmail.com>
2023-04-09 03:39:51 +0300

    edit

--- result.js
+++ result.js
@@ -19,39 +19,53 @@ let q = [
         wrong: 0
 
     }
-]
-let a = q[0].answers;
-let c = q[0].correct;
-let w = q[0].wrong;
+    ,
+    {
+        question:' 2 + 5 = ',
+        answers:['2' , '4' , '7' , '6'],
+        correct:2,
+        wrong: 0
+
+    }
+];
+
+
+// let a = q[1].answers;
+// let c = q[1].correct;
+// let w = q[1].wrong;
 // console.log(q[0].)
 function createCard(arr){
- let card = document.createElement('div');
- card.className = 'card';
+    for (let x = 0; x < arr.length; x++) {
+        let card = document.createElement('div');
+        card.className = 'card';
+        
 
- let title = document.createElement('h3');
- title.textContent = 'Incorrect questions :';
-
- let question = document.createElement('p');
- question.className = 'question';
- question.textContent = arr[0].question;
-
- let answers = document.createElement('ul');
-
- for(let i = 0 ; i < a.length ; i++){
- let item = document.createElement('li');
- item.textContent = a[i];
- if(i == c){
-    item.className = 'correct';
- }
-//  else if (i == w){
-//   item.className = 'wrong'
-//  }
- answers.appendChild(item);
- }
-
- card.append(title , question , answers);
- wrongAns.appendChild(card);
-
+        let title = document.createElement('h3');
+        title.textContent = 'Incorrect questions :';
+        card.append(title);
+        let question = document.createElement('p');
+        question.textContent = arr[x].question;//
+        question.className = 'question';
+        card.append(question);
+        
+        let answers = document.createElement('ul');
+        for(let i = 0 ; i < arr[x].answers.length ; i++){
+            let item = document.createElement('li');
+            item.textContent =arr[x].answers[i] ;
+        if(i == arr[x].correct){
+           item.className = 'correct';
+        }
+        else if(i == arr[x].wrong){
+           item.className = 'wrong'
+        }else{
+            item.className = 'dis'
+        }
+       
+        answers.appendChild(item);
+        }
+        card.append(answers);
+        wrongAns.appendChild(card);
+    }
 }
 
 // let pass = true;
@@ -64,7 +78,7 @@ if(num > 50){
     percantage.textContent = `${num}%`; 
     correctAnswers.textContent = '3'; //
     wrongAnswers.textContent = '2' //  
-    console.log('yes')
+    // console.log('yes')
 }
 
 else{
@@ -81,5 +95,7 @@ else{
     correctAnswers.textContent = '1'; //
     wrongAnswers.textContent = '4' //  
 }
+
+
 createCard(q);
 
